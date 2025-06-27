08cfb54a767994b4ea25921e905b3d4d5c37a938
ignaciourbina <87782759+ignaciourbina@users.noreply.github.com>
2025-06-27 17:20:14 -0400

    Update module_quiz_builder_to_csv.py

--- scripts/module_quiz_builder_to_csv.py
+++ scripts/module_quiz_builder_to_csv.py
@@ -154,7 +154,7 @@ class SectionHeader(WrittenResponse):
         super().__init__(
             title=f"Section ▸ {title}",
             question_text=body_html,
-            points=0,
+            points=0.00000000001, # D2L does not admit zero points items
             html_used=True,
         )
 
